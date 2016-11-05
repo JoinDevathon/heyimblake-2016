19c4d175e9db99c8a08204ac9c3b18b090b8f77e
heyimblake <heyimblake@users.noreply.github.com>
2016-11-05 11:16:51 -0400

    Add events

---
+++ src/main/java/org/devathon/contest2016/events/NewRobotEvent.java
@@ -0,0 +1,28 @@
+package org.devathon.contest2016.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.devathon.contest2016.robotutils.Robot;
+
+/**
+ * Created by heyimblake on 11/5/2016.
+ *
+ * @author heyimblake
+ *         https://heyimblake.me
+ */
+public class NewRobotEvent extends Event {
+
+    private Player player;
+    private Robot robot;
+
+    public NewRobotEvent(Robot robot) {
+        this.player = robot.getPlayer();
+        this.robot = robot;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return getHandlers();
+    }
+}
---
+++ src/main/java/org/devathon/contest2016/events/RemoveRobotEvent.java
@@ -0,0 +1,35 @@
+package org.devathon.contest2016.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.devathon.contest2016.robotutils.Robot;
+
+/**
+ * Created by heyimblake on 11/5/2016.
+ *
+ * @author heyimblake
+ *         https://heyimblake.me
+ */
+public class RemoveRobotEvent extends Event {
+    private Robot robot;
+    private Player player;
+
+    public RemoveRobotEvent(Robot robot) {
+        this.robot = robot;
+        this.player = robot.getPlayer();
+    }
+
+    public Robot getRobot() {
+        return robot;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return getHandlers();
+    }
+}
